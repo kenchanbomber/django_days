02154579a7fb48fc0408f6d7a0b3960163168bbc
kengo hirata <athlete551@icloud.com>
2022-03-03 17:52:02 +0900

    [add] Commentモデルを追加

--- post/models.py
+++ post/models.py
@@ -17,3 +17,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=50)
+    text = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.text} ({self.name})"
--- post/test/test_models.py
+++ post/test/test_models.py
@@ -2,13 +2,15 @@ import datetime
 from django.test import TestCase
 from django.core.files import File
 
-from post.models import Post
+from post.models import Post, Comment
 
 
 class PostModelTest(TestCase):
     def setUp(self):
         self.test_post = Post(title='test post', date=datetime.datetime.now(),
                               content='# title', image=File(file=b'0101'))
+        self.test_comment = Comment(
+            post=self.test_post, name='Test User', text='This post is great!')
 
     def test_be_empty(self):
         posts = Post.objects.all()
@@ -23,3 +25,14 @@ class PostModelTest(TestCase):
         self.test_post.save()
         latest_post = Post.objects.all()[0]
         self.assertEqual(self.test_post.id, latest_post.id)
+
+    def test_be_empty_comment(self):
+        post = self.test_post
+        post.save()
+        self.assertEqual(post.comments.all().count(), 0)
+
+    def test_be_saved_comment(self):
+        self.test_post.save()
+        self.test_comment.save()
+        post = Post.objects.all()[0]
+        self.assertEqual(post.comments.all()[0], self.test_comment)
